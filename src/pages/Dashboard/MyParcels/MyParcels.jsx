11beb084fdc17e35e79c132cb418e80468707280
RafieQt <tajuddinrafi76@gmail.com>
2026-03-28 23:21:09 +0600

    parcel delete option has been added with tanstack

--- src/pages/Dashboard/MyParcels/MyParcels.jsx
+++ src/pages/Dashboard/MyParcels/MyParcels.jsx
@@ -2,17 +2,48 @@ import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 import useAuth from '../../../hooks/useAuth';
 import useAxiosSecure from '../../../hooks/useAxiosSecure';
+import { MdDelete } from "react-icons/md";
+import { MdEditSquare } from "react-icons/md";
+import Swal from 'sweetalert2';
 
 const MyParcels = () => {
     const { user } = useAuth();
     const axiosSecure = useAxiosSecure();
-    const { data: parcels = [] } = useQuery({
+    const { data: parcels = [], refetch} = useQuery({
         queryKey: ['myParcels', user.email],
         queryFn: async () => {
             const res = await axiosSecure.get(`/parcels?email=${user.email}`);
             return res.data;
         }
     });
+
+    const handleDelete = (id) => {
+        console.log(id)
+
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                axiosSecure.delete(`/parcels/${id}`)
+                    .then(res => {
+                        if (res.data.deletedCount) {
+                            refetch();
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "Your parcel has been deleted.",
+                                icon: "success"
+                            })
+                        }
+                    });
+            };
+        });
+    }
     return (
         <div>
             <h2>All my Parcels: {parcels.length}</h2>
@@ -29,21 +60,30 @@ const MyParcels = () => {
                             <th>Sender Name</th>
                             <th>Receiver Name</th>
                             <th>Parcel Cost</th>
+                            <th>Actions</th>
                         </tr>
                     </thead>
                     <tbody>
                         {
-                            parcels.map((parcel,i) => <tr key={parcel._id}>
-                                <th>{i+1}</th>
+                            parcels.map((parcel, i) => <tr key={parcel._id}>
+                                <th>{i + 1}</th>
                                 <td>{parcel.parcelName}</td>
                                 <td>{parcel.parcelType}</td>
                                 <td>{parcel.parcelWeight}</td>
                                 <td>{parcel.senderName}</td>
                                 <td>{parcel.receiverName}</td>
                                 <td>{parcel.cost}</td>
+                                <td>
+                                    <button className="btn btn-square hover:bg-primary mr-2">
+                                        <MdEditSquare />
+                                    </button>
+                                    <button onClick={() => handleDelete(parcel._id)} className="btn btn-circle hover:bg-primary">
+                                        <MdDelete />
+                                    </button>
+                                </td>
                             </tr>)
                         }
-                        
+
                     </tbody>
                 </table>
             </div>
