d38790ffe703009ef9bda3ed5aed0e54d2c01914
RafieQt <tajuddinrafi76@gmail.com>
2026-04-10 21:57:03 +0600

    fetched data from db and design fix

--- src/pages/Dashboard/paymentHistory/PaymentHistory.jsx
+++ src/pages/Dashboard/paymentHistory/PaymentHistory.jsx
@@ -18,7 +18,7 @@ const PaymentHistory = () => {
     return (
         <div>
             <div className='w-322 min-h-176 ml-8 mr-13 mt-8 rounded-2xl bg-white p-8'>
-                <h1 className='text-secondary font-extrabold text-5xl'>Payment History ({payments.length})</h1>
+                <h1 className='text-secondary font-extrabold text-5xl pb-10'>Payment History ({payments.length})</h1>
 
                 <div>
                     <div className="overflow-x-auto">
