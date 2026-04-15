6ff780feebc521e9d674a4b4a862d77b03b01906
RafieQt <tajuddinrafi76@gmail.com>
2026-04-15 23:43:02 +0600

    rider validation has been improved

--- src/pages/rider/Rider.jsx
+++ src/pages/rider/Rider.jsx
@@ -38,17 +38,21 @@ const Rider = () => {
                 <div>
                     <form onSubmit={handleSubmit(handleRider)}>
                         <fieldset className="fieldset w-130">
+                            {/* rider name */}
                             <label className="label text-sm font-semibold">Your Name</label>
                             <input type="text" {...register('riderName', { required: true })} className="w-full input bg-white" placeholder="Name" />
                             {
                                 errors.riderName?.type == "required" && (<p className='text-secondary text-xs'>Rider's name is required</p>)
                             }
+
+                            {/* driving license */}
                             <label className="label text-sm font-semibold">Driving License Number</label>
                             <input type="number" {...register("licenseNumber", { required: true })} className="w-full input bg-white" placeholder="Password" />
                             {
                                 errors.licenseNumber?.type == "required" && (<p className='text-secondary text-xs'>Rider's license number is required</p>)
                             }
 
+                            {/* rider mail */}
                             <label className="label text-sm font-semibold">Email</label>
                             <input type="text" {...register("riderMail", { required: true })} className="w-full input bg-white" placeholder="Email" />
                             {
@@ -85,30 +89,41 @@ const Rider = () => {
                                 errors.riderDistrict && (<p className='text-secondary text-xs'>District is required!</p>)
                             }
 
+                            {/* rider religion */}
                             <label className="label text-sm font-semibold">Your Religion</label>
                             <input type="text" {...register("riderReligion", { required: true })} className="w-full input bg-white" placeholder="Religion" />
                             {
                                 errors.riderReligion?.type == "required" && (<p className='text-secondary text-xs'>Rider's religion is required</p>)
                             }
 
+                            {/* rider NID */}
                             <label className="label text-sm font-semibold">NID no.</label>
-                            <input type="number" {...register("riderNID", { required: true })} className="w-full input bg-white" placeholder="NID number" />
+                            <input type="number" {...register("riderNID", { required: true, minLength:10 })} className="w-full input bg-white" placeholder="NID number" />
                             {
                                 errors.riderNID?.type == "required" && (<p className='text-secondary text-xs'>Rider's NID is required</p>)
                             }
+                            {
+                                errors.riderNID?.type == "minLength" && (<p className='text-secondary text-xs'>Phone Number has be 10 digit</p>)
+                            }
 
+                            {/* rider phone number */}
                             <label className="label text-sm font-semibold">Phone Number</label>
-                            <input type="text" {...register("riderPhone", { required: true })} className="w-full input bg-white" placeholder="Phone Number" />
+                            <input type="text" {...register("riderPhone", { required: true, minLength: 11 })} className="w-full input bg-white" placeholder="Phone Number" />
                             {
                                 errors.riderPhone?.type == "required" && (<p className='text-secondary text-xs'>Rider's phone number is required</p>)
                             }
+                            {
+                                errors.riderPhone?.type == "minLength" && (<p className='text-secondary text-xs'>Phone Number has be 11 digit</p>)
+                            }
 
+                            {/* Registration number */}
                             <label className="label text-sm font-semibold">Bike Registration Number</label>
                             <input type="number" {...register("riderRegNo", { required: true })} className="w-full input bg-white" placeholder="Registration Number" />
                             {
                                 errors.riderRegNo?.type == "required" && (<p className='text-secondary text-xs'>Rider's bike registration number is required</p>)
                             }
 
+                            {/* About Rider */}
                             <label className="label text-sm font-semibold">Tell Us About Yourself</label>
                             <input type="Text" {...register("riderAbout", { required: true })} className="w-full input bg-white" placeholder="Write about yourself" />
                             {
